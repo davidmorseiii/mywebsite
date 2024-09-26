3b532a4bc768e92320bfa74fbf9181f54d018683
davidmorseiii <dmmorse3@gmail.com>
2024-09-26 13:51:46 -0600

    Added practice.js

---
+++ script.js
@@ -0,0 +1,15 @@
+
+//console.log(myName);
+// var - function scoped or global is outisde function
+var myName = "David Morse";
+console.log(myName);
+myName = 30;
+console.log(myName);
+
+// let - block scoped / if outside of a block it's global / use let/const if need to make a variable
+let spouseName = "Emily Morse";
+console.log(spouseName);
+
+// last one
+childName = "David Jr";
+console.log(childName);
